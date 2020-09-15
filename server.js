bcdc4ed1503ac79fa71dbb35832d210decffcd39
Stovencrantz <sknapp143@gmail.com>
2020-09-14 20:18:19 -0400

    Adjusted connection for heroku push

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true, 
-    useFindAndModify: false 
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useInifiedTopology: true
 });
 
 app.use(require("./routes/api-routes"));
